6c370c7bac6cc70720f9e12145fce5606b51d17e
gerson7777 <ggonzalezp3@miumg.edu.gt>
2020-04-03 23:34:26 -0600

    Corregir bug donde si se mandaban carracteres vacios en el login para repartidor se cerraba la app

--- app/src/main/java/gb/paqueteria/gbpaqueteria/MapsActivity.java
+++ app/src/main/java/gb/paqueteria/gbpaqueteria/MapsActivity.java
@@ -32,8 +32,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
-        getSupportActionBar().setCustomView(R.layout.txt_layout);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
