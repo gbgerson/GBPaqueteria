0a313ccd5cf9680990d6aa122da4f327626a1ac9
gerson7777 <ggonzalezp3@miumg.edu.gt>
2020-04-04 22:35:52 -0600

    Librerias de Recycley view y cardview

--- app/src/main/java/gb/paqueteria/gbpaqueteria/repartidor/EntregadasActivity.java
+++ app/src/main/java/gb/paqueteria/gbpaqueteria/repartidor/EntregadasActivity.java
@@ -2,18 +2,19 @@ package gb.paqueteria.gbpaqueteria.repartidor;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import gb.paqueteria.gbpaqueteria.R;
 
 public class EntregadasActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entregadas);
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.txt_titulo);
+
     }
 }
