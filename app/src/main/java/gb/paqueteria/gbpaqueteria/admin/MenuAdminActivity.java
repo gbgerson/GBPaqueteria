921a3c384de623cf2966c29167e3b2076bac50a6
gerson7777 <gersonbreakgonzalez@gmail.com>
2020-04-12 19:12:31 -0600

    creando el Menu para Administrador

--- app/src/main/java/gb/paqueteria/gbpaqueteria/admin/MenuAdminActivity.java
+++ app/src/main/java/gb/paqueteria/gbpaqueteria/admin/MenuAdminActivity.java
@@ -7,13 +7,76 @@ import android.os.Bundle;
 
 import gb.paqueteria.gbpaqueteria.R;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.hitomi.cmlibrary.CircleMenu;
+import com.hitomi.cmlibrary.OnMenuSelectedListener;
+import com.hitomi.cmlibrary.OnMenuStatusChangeListener;
+
+
+
 public class MenuAdminActivity extends AppCompatActivity {
+    CircleMenu circleMenu;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu_admin);
+        setContentView(R.layout.activity_menurepartidorctivity);
+        // para traer nuestro titulo de la app centrado
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.txt_loginadmin);
+        // casteando nuestro circle menu
+        circleMenu = (CircleMenu) findViewById(R.id.circle_menu);
+        //
+        circleMenu.setMainMenu(Color.parseColor("#FA8258"), R.drawable.menutres, R.drawable.cancelar)
+                .addSubMenu(Color.parseColor("#0FF45C"), R.drawable.subir)
+                .addSubMenu(Color.parseColor("#80F816"), R.drawable.remover)
+                .setOnMenuSelectedListener(new OnMenuSelectedListener() {
+
+                    @Override
+                    public void onMenuSelected(int index) {
+                        if (index==0){
+                            new Handler().postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Intent intent = new Intent(MenuAdminActivity.this, RegistrarOfertasActivity.class);
+                                     startActivity(intent);
+                                }
+                            },600);
+
+                        }
+
+                        if (index==1){
+                            new Handler().postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Intent intent = new Intent(MenuAdminActivity.this, RemoverOfertasActivity.class);
+                                    startActivity(intent);
+                                }
+                            },600);
+
+                        }
+
+
+
+                    }
+
+                }).setOnMenuStatusChangeListener(new OnMenuStatusChangeListener() {
+
+            @Override
+            public void onMenuOpened() {}
+
+            @Override
+            public void onMenuClosed() {}
+
+        });
     }
 }
---
+++ app/src/main/java/gb/paqueteria/gbpaqueteria/admin/RegistrarOfertasActivity.java
@@ -0,0 +1,16 @@
+package gb.paqueteria.gbpaqueteria.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import gb.paqueteria.gbpaqueteria.R;
+
+public class RegistrarOfertasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar_ofertas);
+    }
+}
